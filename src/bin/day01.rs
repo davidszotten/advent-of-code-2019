4d2109fd469b3f9e00ed56094d265874d71cb816
David Szotten <davidszotten@gmail.com>
2019-12-02 21:31:43 +0000

    fmt

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -8,16 +8,16 @@ fn part1(input: &str) -> Result<i32> {
     Ok(input
         .split('\n')
         .filter_map(|x| x.parse::<i32>().ok())
-        .map(|x| x/3 -2)
+        .map(|x| x / 3 - 2)
         .sum())
 }
 
 fn recurse_fuel(mass: i32) -> i32 {
     let mut sum = 0;
-    let mut fuel = mass /3 - 2;
+    let mut fuel = mass / 3 - 2;
     while fuel > 0 {
         sum += fuel;
-        fuel = fuel /3 - 2;
+        fuel = fuel / 3 - 2;
     }
     sum
 }
