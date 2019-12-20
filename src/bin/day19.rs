ab5b7ca06ecd0c902777f79ba73535404e93e2e1
David Szotten <davidszotten@gmail.com>
2019-12-20 13:09:04 +0000

    day19 p2 too slow

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -7,31 +7,6 @@ fn main() -> Result<()> {
     dispatch(&part1, &part2)
 }
 
-fn part1(input: &str) -> Result<i32> {
-    let mut cpu = Cpu::from_str(input);
-    cpu.run()?;
-    let mut sum = 0;
-    for x in 0..50 {
-        for y in 0..50 {
-            let mut cpu = cpu.clone();
-            cpu.enqueue_input(x);
-            cpu.enqueue_input(y);
-
-            match cpu.run()? {
-                CpuState::Output(value) => match value {
-                    0 => {}
-                    1 => {
-                        sum += 1;
-                    }
-                    _ => unreachable!("invalid output value"),
-                },
-                s => panic!("({},{}), state: {:?}", x, y, s),
-            }
-        }
-    }
-    Ok(sum)
-}
-
 struct Map {
     cpu: Cpu,
     data: HashMap<Coor, bool>,
@@ -45,7 +20,8 @@ impl Map {
         Self { cpu, data }
     }
 
-    fn get(&self, coor: Coor) -> bool {
+    fn get(&self, x: i64, y: i64) -> bool {
+        let coor = Coor::new(x, y);
         if let Some(&value) = self.data.get(&coor) {
             return value;
         }
@@ -64,10 +40,44 @@ impl Map {
     }
 }
 
-fn part2(input: &str) -> Result<i32> {
+fn part1(input: &str) -> Result<i32> {
+    let map = Map::new(input);
     let mut sum = 0;
     for x in 0..50 {
-        for y in 0..50 {}
+        for y in 0..50 {
+            if map.get(x, y) {
+                sum += 1
+            }
+        }
     }
     Ok(sum)
+}
+
+fn find_square(input: &str, size: usize) -> Result<usize> {
+    let map = Map::new(input);
+    let mut distance = 1;
+    'main: loop {
+        for start_x in 0..distance {
+            for start_y in 0..distance {
+                let mut square = true;
+                'square: for x in start_x..(start_x + size) {
+                    for y in start_y..(start_y + size) {
+                        if !map.get(x as i64, y as i64) {
+                            square = false;
+                            break 'square;
+                        }
+                    }
+                }
+                if square {
+                    break 'main Ok(start_x * 10_000 + start_y);
+                }
+            }
+        }
+        distance += 1;
+        dbg!(distance);
+    }
+}
+
+fn part2(input: &str) -> Result<usize> {
+    find_square(input, 100)
 }
