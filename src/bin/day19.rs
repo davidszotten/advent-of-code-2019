dfd39b75fae2af8c9ae44ca5d7ce39b8280a0c7c
David Szotten <davidszotten@gmail.com>
2019-12-20 20:01:46 +0000

    day19 p2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -20,7 +20,7 @@ impl Map {
         Self { cpu, data }
     }
 
-    fn get(&self, x: i64, y: i64) -> bool {
+    fn get(&mut self, x: i64, y: i64) -> bool {
         let coor = Coor::new(x, y);
         if let Some(&value) = self.data.get(&coor) {
             return value;
@@ -29,19 +29,21 @@ impl Map {
         cpu.enqueue_input(coor.x);
         cpu.enqueue_input(coor.y);
 
-        match cpu.run().expect("run failed") {
+        let value = match cpu.run().expect("run failed") {
             CpuState::Output(value) => match value {
                 0 => false,
                 1 => true,
                 _ => unreachable!("invalid output value"),
             },
             s => panic!("state: {:?}", s),
-        }
+        };
+        self.data.insert(coor, value);
+        value
     }
 }
 
-fn part1(input: &str) -> Result<i32> {
-    let map = Map::new(input);
+fn part1(input: &str) -> Result<i64> {
+    let mut map = Map::new(input);
     let mut sum = 0;
     for x in 0..50 {
         for y in 0..50 {
@@ -53,31 +55,67 @@ fn part1(input: &str) -> Result<i32> {
     Ok(sum)
 }
 
-fn find_square(input: &str, size: usize) -> Result<usize> {
-    let map = Map::new(input);
-    let mut distance = 1;
-    'main: loop {
-        for start_x in 0..distance {
-            for start_y in 0..distance {
-                let mut square = true;
-                'square: for x in start_x..(start_x + size) {
-                    for y in start_y..(start_y + size) {
-                        if !map.get(x as i64, y as i64) {
-                            square = false;
-                            break 'square;
-                        }
-                    }
+fn _display(map: &Map, distance: i64) {
+    for y in 0..distance {
+        for x in 0..distance {
+            print!(
+                "{}",
+                match map.data.get(&Coor::new(x, y)) {
+                    None => " ",
+                    Some(true) => "#",
+                    Some(false) => ".",
                 }
-                if square {
-                    break 'main Ok(start_x * 10_000 + start_y);
-                }
-            }
+            )
         }
-        distance += 1;
-        dbg!(distance);
+        print!("\n");
     }
+    println!("\n");
 }
 
-fn part2(input: &str) -> Result<usize> {
+fn find_square(input: &str, size: i64) -> Result<i64> {
+    let mut map = Map::new(input);
+
+    let mut prev_x = 0;
+    'y: for start_y in 0.. {
+        // display(&map, start_y + 1);
+
+        let mut start_x = prev_x;
+        while !map.get(start_x, start_y) {
+            start_x += 1;
+            if start_x > start_y * 4 {
+                continue 'y;
+            }
+        }
+        prev_x = start_x;
+        while map.get(start_x + size - 1, start_y) {
+            if !(map.get(start_x, start_y)
+                && map.get(start_x, start_y + size - 1)
+                && map.get(start_x + size - 1, start_y + size - 1))
+            {
+                start_x += 1;
+                continue;
+            }
+            let mut square = true;
+            'square: for x in start_x..(start_x + size) {
+                for y in start_y..(start_y + size) {
+                    if !map.get(x, y) {
+                        square = false;
+                        break 'square;
+                    }
+                }
+            }
+            if square {
+                // display(&map, start_y + (1 + size) * 2);
+                return Ok(start_x * 10_000 + start_y);
+            }
+            start_x += 1;
+        }
+
+        // dbg!(start_y);
+    }
+    Ok(0)
+}
+
+fn part2(input: &str) -> Result<i64> {
     find_square(input, 100)
 }
